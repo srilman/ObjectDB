c268653bfda6c7b3255a63d226146a81a6ba12f7
Srinivas Lade <srinulade1@gmail.com>
2018-07-22 00:24:47 -0700

    Setting Up Repo For PyPi Publishing and Open Source Contribution
    Adding a License to the repo. Useful for open source projects.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ObjectDB
+Simple File-Based Database for Storing Data Structures Into Build-In Python Formats.
+
+# Introduction
+This project is heavily inspired by TinyDB, which is also a file-based storage manager. However, it is intended for storing single values in a dictionary format, while ObjectDB stores complex data structures in more of a relational database style.
--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="objectdb",
       version="0.0.1",
-      description="Simple Database / File Storage Manager" \ 
-                  "for Python Class Objects",
-      author="Srinu L",
+      author="Srinivas Lade",
       author_email="srinulade1@gmail.com",
       license="MIT",
-      packages=["objectdb"]
+      description="Simple File-Based Database for Data Structures",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/srilman/ObjectDB",
+      packages=["object_db"],
+      classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ),
       )
